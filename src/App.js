e443c70321654a959f0cccf4db771d9ed6fb9db6
André Luiz <43641969+aluiz815@users.noreply.github.com>
2021-01-25 01:04:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import Aboutme from './components/Aboutme';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-import {BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <>
-    <BrowserRouter>
       <GlobalStyle/>
       <Header/>
       <Aboutme/>
       <Skills/>
       <Projects/>
       <Footer/>
-    </BrowserRouter>
     </>
   );
 }

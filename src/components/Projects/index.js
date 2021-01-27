da5c8fc77fda17cc4cf3da6dd1aa3ceac30f70c9
Andre Luiz <aluiz815@gmail.com>
2021-01-27 04:14:04 -0300

    Projects urls fixed

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,24 +7,24 @@ function Projects() {
       <h1 style={{textAlign:'center',textTransform:'uppercase',fontWeight:'bold'}}>PROJETOS</h1>
       <Container id="projects">
         <ProjectsContainer>
-          <ProjectsWrapper>
-            <ProjectInfo href="https://github.com/aluiz815/proffy">
+          <ProjectsWrapper href="https://github.com/aluiz815/proffy">
+            <ProjectInfo>
               <ProjectImg src={Proffy_img}/>
             </ProjectInfo>
             <ProjectText>Proffy</ProjectText>
           </ProjectsWrapper>
         </ProjectsContainer>
         <ProjectsContainer>
-          <ProjectsWrapper>
-            <ProjectInfo href="https://github.com/aluiz815/be-the-hero">
+          <ProjectsWrapper href="https://github.com/aluiz815/be-the-hero">
+            <ProjectInfo>
               <ProjectImg src="https://raw.githubusercontent.com/aluiz815/be-the-hero/7bf66f230bde58c7dffba988e1a502272cbba629/frontend/src/assets/logo.svg"/>
             </ProjectInfo>
             <ProjectText>Be The Hero</ProjectText>
           </ProjectsWrapper>
         </ProjectsContainer>
         <ProjectsContainer>
-          <ProjectsWrapper>
-            <ProjectInfo href="https://github.com/aluiz815/picpay_clone">
+          <ProjectsWrapper href="https://github.com/aluiz815/picpay_clone">
+            <ProjectInfo>
               <ProjectImg src="https://github.com/aluiz815/picpay_clone/blob/master/assets/01@3x.png?raw=true"/>
             </ProjectInfo>
             <ProjectText>Picpay Clone</ProjectText>

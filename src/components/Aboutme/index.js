54ff68f3e8f705fda75cbf37a7f97207c5e7dc02
Andre Luiz <aluiz815@gmail.com>
2021-02-25 03:25:16 -0300

    Urls fixed

--- src/components/Aboutme/index.js
+++ src/components/Aboutme/index.js
@@ -10,7 +10,7 @@ function Aboutme() {
         <p>Programador apaixonado cursando Ciência da Computação na FG. Programei diversos projetos por conta própria como hobby. <br/>
         Busco um trabalho em que eu possa utilizar meu conhecimento adquirido tanto em cursos quanto em meus projetos pessoais.</p>
         <ButtonsContainer>
-          <a href="https://drive.google.com/file/d/10QRBHoXLzsJg7nKn4FLn9nfOcGiczJtL/view?usp=sharing">Meu Curriculo</a>
+          <a href="https://drive.google.com/file/d/1xGJdkf8_lbj4-lXtfFOPD7rQGfqiVxuw/view">Meu Curriculo</a>
           <a href="Whatsapp://send?phone=5581982505544">Entre em contato</a>
         </ButtonsContainer>
       </Info>

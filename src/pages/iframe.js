7e550ea2221d0a6c17bd7381b0faba5edcf38235
Andre Luiz <aluiz815@gmail.com>
2021-09-13 20:24:58 -0300

    Onesignal subdomain test 8

--- src/pages/iframe.js
+++ src/pages/iframe.js
@@ -3,8 +3,7 @@ import React, { useEffect } from 'react'
 export default function Iframe() {
     const OneSignal = window.OneSignal;
     useEffect(()=>{
-        const loadOne = async () => {
-            window.OneSignal = await window.OneSignal || [];
+            window.OneSignal =  window.OneSignal || [];
             OneSignal.push(function() {
             OneSignal.init({
               appId: "f847abb8-3282-4a84-8e14-533f3649da30",
@@ -13,28 +12,17 @@ export default function Iframe() {
                 enable: true,
               },
             });
-          });
-           // Make sure OneSignal Init code is on this page
-        function bindEvent(element, eventName, eventHandler) {
-            element.addEventListener(eventName, eventHandler, false);
-        }
-        // Accessed within iframe on subdomain.site
-        // Sends a message to mainsite
-        var sendMessage = function (msg) {
-            console.log(`2 Mainsite is Sending Message to subdomain.site ${msg}`)
-            // postMessage: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
-            window.parent.postMessage(msg, window.location.origin);
-        };
-        bindEvent(window, 'load', function (e) {
-            OneSignal.push(function() {
             OneSignal.isPushNotificationsEnabled(function(isEnabled) {
                 console.log(`1 subdomain.site iframe checking subscription from mainsite, it is ${isEnabled}`)
                 sendMessage(isEnabled)
             });
-            });
-        });
-            }
-            loadOne()
+          });
+        // Sends a message to mainsite
+        var sendMessage = function (msg) {
+            console.log(`2 Mainsite is Sending Message to subdomain.site ${msg}`)
+            // postMessage: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+            window.parent.postMessage(msg, "https://pizza.andredeveloper.com.br");
+        };
     },[OneSignal])
     return (
         <div>

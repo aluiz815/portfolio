6dbe9a4c70f61a76bcf548fef9984e28e9de75fb
Andre Luiz <aluiz815@gmail.com>
2021-09-13 19:13:41 -0300

    Onesignal subdomain test 2

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Iframe from './pages/iframe';
-import { useEffect } from 'react';
 function App() {
-  const OneSignal = window.OneSignal;
-  useEffect(()=>{
-    const loadOne = async () => {
-    window.OneSignal = await window.OneSignal || [];
-    OneSignal.push(function() {
-    OneSignal.init({
-      appId: "f847abb8-3282-4a84-8e14-533f3649da30",
-      safari_web_id: "web.onesignal.auto.2cd5950f-b486-4c84-8005-0f30059d0b0c",
-      notifyButton: {
-        enable: true,
-      }
-    });
-  });
-    }
-    loadOne()
-  },[OneSignal])
   return (
     <>
       <GlobalStyle/>

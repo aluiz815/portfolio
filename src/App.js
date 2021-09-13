0ff321b6c2ad1baeda7446168987e3df7ec2c166
Andre Luiz <aluiz815@gmail.com>
2021-09-13 17:39:41 -0300

    OneSignal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import Aboutme from './components/Aboutme';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "f847abb8-3282-4a84-8e14-533f3649da30",
+      safari_web_id: "web.onesignal.auto.2cd5950f-b486-4c84-8005-0f30059d0b0c",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+  },[])
   return (
     <>
       <GlobalStyle/>

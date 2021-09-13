7676647420bdeee112cbfa5d5e913597fdb664ea
Andre Luiz <aluiz815@gmail.com>
2021-09-13 18:16:47 -0300

    OneSignal v2

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import { useEffect } from 'react';
+const OneSignal = window.OneSignal;
 function App() {
   useEffect(()=>{
-    window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
+    const loadOne = async () => {
+      window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
     OneSignal.init({
       appId: "f847abb8-3282-4a84-8e14-533f3649da30",
       safari_web_id: "web.onesignal.auto.2cd5950f-b486-4c84-8005-0f30059d0b0c",
@@ -17,6 +19,8 @@ function App() {
       },
     });
   });
+    }
+    loadOne()
   },[])
   return (
     <>
